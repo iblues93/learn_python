1ef4c229efb85fb4493c04463ea64b0e7d7fa182
iblues93 <oxitachixo@hotmail.com>
2018-05-30 13:29:45 +0800

    Update string_formatting.py

--- lpthw/string_formatting.py
+++ lpthw/string_formatting.py
@@ -4,12 +4,12 @@ Exercise 5
 
 my_string = "Hello World!"
 dates = [5,7,9]
-exchange_rate = 1.5
+exchange_rate = 1.5/7
 
 #formatting string
 
-formated_string = f"{my_string} Today is {dates[1]}th and the exchange rate is {exchange_rate}."
-formated_string2 = "{0} Today is {1[1]}th and the exchange rate is {2:.2f}.".format(my_string,dates,exchange_rate/7)
+formated_string = f"{my_string} Today is {dates[1]}th and the exchange rate is {round(exchange_rate,2)}."
+formated_string2 = "{0} Today is {1[1]}th and the exchange rate is {2:.2f}.".format(my_string,dates,exchange_rate)
 
 print(formated_string)
 
